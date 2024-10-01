6e5b92286481d24423e96c46fccc50a3c8a3cd2e
raymond301 <raymond.moore301@gmail.com>
2024-10-01 15:33:01 -0500

    add parameters to input quantification formatting

--- templates/merge_files.py
+++ templates/merge_files.py
@@ -3,17 +3,19 @@
 import sys, os
 import pandas as pd
 
-slide_by_prefix=True
+slide_by_prefix = ${params.slide_contains_prefix}
+input_extension='${params.quant_file_extension}'
+input_delimiter='${params.quant_file_delimiter}'
 
 def merge_tab_delimited_files(directory_path, excld):
 	# List all files in the directory
-	files = [f for f in os.listdir(directory_path) if f.endswith('.tsv')]
+	files = [f for f in os.listdir(directory_path) if f.endswith(input_extension)]
 
 	# Read and concatenate all tab-delimited files into a single DataFrame
 	dataframes = []
 	for file in files:
 		file_path = os.path.join(directory_path, file)
-		df = pd.read_csv(file_path, sep='\t')
+		df = pd.read_csv(file_path, sep=input_delimiter)
 		if excld != '':
 			df = df[df.columns.drop(list(df.filter(regex='('+excld+')')))]
 		if slide_by_prefix:
